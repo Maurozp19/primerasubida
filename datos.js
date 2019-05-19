78d8a07b598c86c0d9d463923d32bb9ca5f38638
maurozp19 <mauricio.zapata.p@gmail.com>
2019-05-19 15:20:54 -0500

    proyecto1

---
+++ datos.js
@@ -0,0 +1,22 @@
+const opciones = {
+
+    nombre: {
+        alias: 'n',
+        demand: true
+    },
+    matematicas: {
+        alias: 'm',
+        demand: true
+    },
+    ingles: {
+        alias: 'i',
+        demand: true
+    },
+    programacion: {
+        alias: 'p',
+        demand: true
+    }
+}
+
+let obtenerPromedio = (nota_uno, nota_dos, nota_tres);
+
